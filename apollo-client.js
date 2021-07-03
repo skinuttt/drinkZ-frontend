1fc58549b90307c64e99313ff342655c9a5c33ed
Steve Kinuthia <s.kinuttt@gmail.com>
2021-07-03 19:34:10 +0300

    First commit

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://drinkz-juja.herokuapp.com/graphql",
+  uri: "https://drinkz-back.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
